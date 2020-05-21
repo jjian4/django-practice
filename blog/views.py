2d99eca62fee45a443f662d3afdf4f45e2258f04
jjian4 <jjian@umich.edu>
2020-05-21 18:33:42 -0400

    Templates

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'James',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'Aug 27, 2018'
+    },
+    {
+        'author': 'James',
+        'title': 'Blog post 2',
+        'content': '2nd post content',
+        'date_posted': 'Aug 28, 2018'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
